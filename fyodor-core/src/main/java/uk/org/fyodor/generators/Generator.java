28c863455b0ab005fb5fbd55d703862c4293a3f8
Andrew Morgan <andrew.morgan@ink-global.com>
2014-12-11 23:43:55 +0000

    Added stream method to the generator interface

--- fyodor-core/src/main/java/uk/org/fyodor/generators/Generator.java
+++ fyodor-core/src/main/java/uk/org/fyodor/generators/Generator.java
@@ -1,6 +1,12 @@
 package uk.org.fyodor.generators;
 
+import java.util.stream.Stream;
+
 public interface Generator<T> {
 
     public T next();
+
+    default Stream<T> stream() {
+        return Stream.generate(Generator.this::next);
+    }
 }
